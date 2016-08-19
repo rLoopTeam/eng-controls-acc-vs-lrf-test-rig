b62830ead2a5723886ca781aee266c69d02c37cf
idillia <mila.antonova@gmail.com>
2016-08-19 16:36:26 -0700

    clean up code to display distance from laser

--- LRF/LRF.ino
+++ LRF/LRF.ino
@@ -1,6 +1,6 @@
 /*
  * Laser range finder code for the accelerometer test rig
- * By Paul L (mightyboat)
+ * By Paul L (mightyboat) & Mila A (scrappymacgyver)
  * -------------------------------------------------------
  * LRF useful parameters
  * 0x1B - esc - go in configruation mode
@@ -14,45 +14,49 @@
  *  - 0    OFF
  */
 
-bool started = false;
 int byteRead = 0;
-int CMD_LASER_ON[3] = {2, 0x4F, 0x1};       //turn laser off
-int CMD_LASER_OFF[3] = {2, 0x4F, 0x0};      //turn laser on
-int CMD_CONFIG[2] = {1, 0X1b};              //config mode
-int CMD_SINGLE_READING[2] = {1, 0x63};      //get single readin
-int CMD_CONTINUOUS_READING[2] = {1, 0x43};  //get reading continuously
-int CMD_GET_ERROR[2] = {1, 0x64};           //get errors
+int CMD_LASER_ON[3] = {2, 0x4F, 0x31};      //"O1" turn laser on
+int CMD_LASER_OFF[3] = {2, 0x4F, 0x30};     //"O0" turn laser off
+// int CMD_CONFIG[2] = {1, 0x1B};              //"<esc>" config mode
+// int CMD_SINGLE_READING[2] = {1, 0x63};      //"c" get single reading 
+int CMD_CONTINUOUS_READING[2] = {1, 0x43};  //"C" get reading continuously
+int CMD_MEASUREMENT_MODE_CONTINUOUS_ASCII[3] = {2, 0x4D, 0x31};  //"M1" continuous measurement in ascii mode 
+int CMD_GET_ERROR[2] = {1, 0x64};           //"d" get errors
 
 void setup() {                
   Serial.begin(9600);
   Serial3.begin(9600);
   delay(10);
+
+  send_command(CMD_LASER_OFF);
+  send_command(CMD_LASER_ON);
+  send_command(CMD_CONTINUOUS_READING);
+  send_command(CMD_MEASUREMENT_MODE_CONTINUOUS_ASCII);
 }
 
 void loop() {
-  if (!started){
-    send_command(CMD_CONFIG);
-    send_command(CMD_LASER_ON);
-    send_command(CMD_SINGLE_READING);
-  }
   
-  if (Serial3.available()) {
+  while (Serial3.available()) {
     byteRead = Serial3.read();
-    Serial.print("Distance: "); Serial.println(byteRead);
+    if(byteRead == 10 || byteRead2 == 13) {
+      Serial.println('\n');
+    } else {
+      Serial.print(char(byteRead));  
+    }    
   } else {
-    //send_command(CMD_GET_ERROR);
+    send_command(CMD_GET_ERROR);
   }
-  started = true;
+  delay(10);
 }
 
-/*
- * Send command to laser range finder functions
- */
+// Command wrapper <esc>...<cr> where <esc> is (0x1B) and <cr> is (0xD)
 void send_command(int command[]){
-  Serial3.write(0x1B);
   Serial3.write(0x1B);
   for (int b=1; b <= command[0]; b++) {
     Serial3.write(command[b]);
   }
-  Serial3.write(0xD); //end command
+  Serial3.write(0xD); 
 }
+
+
+
